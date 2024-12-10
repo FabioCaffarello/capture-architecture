e098950a25c1b7af737037d3b311724c84e0ca0d
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-12-10 00:04:41 +0000

    feat: setup husky

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,10 +7,10 @@ npm install || { echo "npm install failed"; exit 1; }
 echo "npm install succeeded"
 
 
-# # # Setting husky
-# npm run prepare || { echo "npm run prepare failed"; exit 1; }
+# # Setting husky
+npm run prepare || { echo "npm run prepare failed"; exit 1; }
 
-# echo "npm run prepare succeeded"
+echo "npm run prepare succeeded"
 
 
 # Setting golang
